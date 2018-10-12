eaf846ddb52c58caafcb1a32e4ff850218b6f487
hastere <hastere@crimson.ua.edu>
2018-10-12 10:15:34 -0500

    made changes limiting the dept and number of estimators in the models for pt1, preventing the model from overfitting

--- Lab2/part1/part_1.py
+++ Lab2/part1/part_1.py
@@ -13,38 +13,40 @@ def main():
     #load data
     train = pd.read_csv('train.csv')
     test = pd.read_csv('test.csv')
-    target = train["target"]
-    features = list(train.columns[3:])
-    catData = train[features]
-    dataTree = tree.DecisionTreeClassifier()
-    dataTree = dataTree.fit(catData, target)
-    predictionTest = dataTree.predict(test[list(test.columns[3:])])
-    predictionTrain = dataTree.predict(catData)
-
+    trainTarget = train["target"]
+    trainFeatureNames = list(train.columns[3:])
+    trainFeatures = train[trainFeatureNames]
+    testTarget = test["target"]
+    testFeatureNames = list(test.columns[3:])
+    testFeatures = test[testFeatureNames]
+    #TODO catagorical columns should be converted to catagorical type
+    dataTree = tree.DecisionTreeClassifier(max_depth = 10)
+    dataTree = dataTree.fit(trainFeatures, trainTarget)
+    testPrediction = dataTree.predict(testFeatures)
+    trainPrediction = dataTree.predict(trainFeatures)
+    print "\nNow using decision trees"
     #Answer to question 1.i
-    trainConfusion = confusion_matrix(train["target"], predictionTrain)
-    testConfusion = confusion_matrix(test["target"], predictionTest)
-    print "Training set confusion matrix"
+    trainConfusion = confusion_matrix(trainTarget, trainPrediction)
+    testConfusion = confusion_matrix(testTarget, testPrediction)
+    print "\nTraining set confusion matrix"
     print trainConfusion
-    print "Test set confusion matrix"
+    print "\nTest set confusion matrix"
     print testConfusion
     #Answer to question 1.ii
-
-    print "Training set prediction, recal, and f-score"
-    print precision_recall_fscore_support(train["target"], predictionTrain, average='binary')[:3]
-
+    print "\nTraining set prediction, recal, and f-score"
+    print precision_recall_fscore_support(trainTarget, trainPrediction, average='binary')[:3]
     #Answer to question 1.iii
-    print "Test set prediction, recall, and f-score"
-    print precision_recall_fscore_support(test["target"], predictionTest, average='binary')[:3]
+    print "\nTest set prediction, recall, and f-score"
+    print precision_recall_fscore_support(testTarget, testPrediction, average='binary')[:3]
 
     #Answer to question 1.iv
-    print "precision and recall decrease between the training data and test data."
+    print "\nprecision and recall decrease between the training data and test data."
     print "this is because the decision tree was trained on the training data, which"
     print "resulted in a perfect fit to that specific set. this is an example of why"
-    print "you shouldn't train a model on test data"
+    print "you shouldn't train a model on test data\n"
 
     #Answer to question 1.v
-    print "for the train data"
+    print "\nfor the train data"
     x = (trainConfusion[1][1])
     y = (x + trainConfusion[1][0])
     print "\tTrue Positive Rate = " + str(float(x)/float(y))
@@ -61,39 +63,39 @@ def main():
     print "\tFalse Positive Rate = " + str(float(x)/float(y))
 
     #Ansrew to question 1.vii
-    print "again, testing on the training data results in perfect classification"
+    print "\nagain, testing on the training data results in perfect classification"
     print "of training data, as the model is based off of the test data. The test"
-    print "data is different, which exposes some errors in prediction"
+    print "data is different, which exposes some errors in prediction\n"
 
     #Answer to question 1.viii
     print "Due to variance in samples the recall value is a better way of evaluating"
     print "performance"
 
-    print "Now using RandomForestClassifier as a model"
-    randomForest = RandomForestClassifier(n_estimators = 100, random_state = 42)
-    randomForest.fit(catData, target);
+    print "\nNow using RandomForestClassifier as a model"
+    randomForest =  RandomForestClassifier(n_estimators = 10, random_state = 42)
+    randomForest.fit(trainFeatures, trainTarget);
     predictionTest = randomForest.predict(test[list(test.columns[3:])])
-    predictionTrain = randomForest.predict(catData)
+    predictionTrain = randomForest.predict(trainFeatures)
 
     #Answer to question 2.i
     trainConfusion = confusion_matrix(train["target"], predictionTrain)
     testConfusion = confusion_matrix(test["target"], predictionTest)
-    print "Training set confusion matrix"
+    print "\nTraining set confusion matrix"
     print trainConfusion
-    print "Test set confusion matrix"
+    print "\nTest set confusion matrix"
     print testConfusion
-    print "Training set prediction, recal, and f-score"
+    print "\nTraining set prediction, recal, and f-score"
     print precision_recall_fscore_support(train["target"], predictionTrain, average='binary')[:3]
 
     #Answer to question 1.iii
-    print "Test set prediction, recall, and f-score"
+    print "\nTest set prediction, recall, and f-score"
     print precision_recall_fscore_support(test["target"], predictionTest, average='binary')[:3]
 
     #Answer to question 1.iv
-    print "precision and recall decrease between the training data and test data."
+    print "\nprecision and recall decrease between the training data and test data."
     print "this is because the decision tree was trained on the training data, which"
     print "resulted in a perfect fit to that specific set. this is an example of why"
-    print "you shouldn't train a model on test data"
+    print "you shouldn't train a model on test data\n"
 
     #Answer to question 1.v
     print "for the train data"
@@ -104,7 +106,7 @@ def main():
     y = (x + trainConfusion[0][0])
     print "\tFalse Positive Rate = " + str(float(x)/float(y))
     #Answer to question 1.vi
-    print "for the test data"
+    print "\nfor the test data"
     x = (testConfusion[1][1])
     y = (x + testConfusion[1][0])
     print "\tTrue Positive Rate = " + str(float(x)/float(y))
@@ -113,9 +115,9 @@ def main():
     print "\tFalse Positive Rate = " + str(float(x)/float(y))
 
     #Ansrew to question 1.vii
-    print "again, testing on the training data results in perfect classification"
+    print "\nagain, testing on the training data results in perfect classification"
     print "of training data, as the model is based off of the test data. The test"
-    print "data is different, which exposes some errors in prediction"
+    print "data is different, which exposes some errors in prediction\n"
 
 
     return 0

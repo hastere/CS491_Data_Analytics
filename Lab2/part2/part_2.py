c2fbad5c5fdbfeae10e392b6e9fd74009908305e
hastere <hastere@crimson.ua.edu>
2018-10-18 14:00:22 -0500

    added changes to the ann allowing for it to actually work

--- Lab2/part2/part_2.py
+++ Lab2/part2/part_2.py
@@ -9,38 +9,46 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
-
+from sklearn.preprocessing import MinMaxScaler
 def main():
     #load data
     train = pd.read_csv('part2Train.csv', header=None)
     test = pd.read_csv('part2Test.csv', header=None)
+    scaler = MinMaxScaler()
     target = train[4]
-    features = list(train.columns[:4])
-    catData = train[features]
+    validation = test[4]
+    #scale it
+    scaledTrain = scaler.fit_transform(train[list(train.columns[:4])])
+    scaledTest = scaler.fit_transform(test[list(train.columns[:4])])
+    trainFeatures = pd.DataFrame(scaledTrain)
+    testFeatures = pd.DataFrame(scaledTest)
+
+    # testFeatures = test[list(test.columns[:4])]
+    # trainFeatures = train[list(train.columns[:4])]
     #decision tree
-    dataTree = tree.DecisionTreeClassifier()
-    dataTree = dataTree.fit(catData, target)
-    dataTreePrediction = dataTree.predict(test[list(test.columns[:4])])
-    dataTreeConfusion = confusion_matrix(test[4], dataTreePrediction)
-    dataTreeMetrics = precision_recall_fscore_support(test[4], dataTreePrediction, average='binary')[:3]
+    dataTree = tree.DecisionTreeClassifier(random_state=69)
+    dataTree = dataTree.fit(trainFeatures, target)
+    dataTreePrediction = dataTree.predict(testFeatures)
+    dataTreeConfusion = confusion_matrix(validation, dataTreePrediction)
+    dataTreeMetrics = precision_recall_fscore_support(validation, dataTreePrediction, average='binary')[:3]
     #svm
-    svmModel = svm.LinearSVC()
-    svmModel = svmModel.fit(catData, target)
-    svmPrediction = svmModel.predict(test[list(test.columns[:4])])
-    svmConfusion = confusion_matrix(test[4], svmPrediction)
-    svmMetrics = precision_recall_fscore_support(test[4], svmPrediction, average='binary')[:3]
+    svmModel = svm.LinearSVC(random_state=69)
+    svmModel = svmModel.fit(trainFeatures, target)
+    svmPrediction = svmModel.predict(testFeatures)
+    svmConfusion = confusion_matrix(validation, svmPrediction)
+    svmMetrics = precision_recall_fscore_support(validation, svmPrediction, average='binary')[:3]
     #logistic regression
-    logReg = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
-    logReg = logReg.fit(catData, target)
-    logRegPrediction = logReg.predict(test[list(test.columns[:4])])
-    logRegConfusion = confusion_matrix(test[4], logRegPrediction)
-    logRegMetrics = precision_recall_fscore_support(test[4], logRegPrediction, average='binary')[:3]
+    logReg = LogisticRegression(random_state=69, solver='lbfgs', multi_class='multinomial')
+    logReg = logReg.fit(trainFeatures, target)
+    logRegPrediction = logReg.predict(testFeatures)
+    logRegConfusion = confusion_matrix(validation, logRegPrediction)
+    logRegMetrics = precision_recall_fscore_support(validation, logRegPrediction, average='binary')[:3]
     #artificial neural net
-    annModel = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-    annModel = annModel.fit(catData, target)
-    annPrediction = annModel.predict(test[list(test.columns[:4])])
-    annConfusion = confusion_matrix(test[4], annPrediction)
-    annMetrics = precision_recall_fscore_support(test[4], annPrediction, average='binary')[:3]
+    annModel = MLPClassifier(solver='lbfgs', alpha=1e-5, random_state=69)
+    annModel = annModel.fit(trainFeatures, target)
+    annPrediction = annModel.predict(testFeatures)
+    annConfusion = confusion_matrix(validation, annPrediction)
+    annMetrics = precision_recall_fscore_support(validation, annPrediction, average='binary')[:3]
     print "\n"
     #Answer to q 3.i
     print "Data Tree confusion matrix"
